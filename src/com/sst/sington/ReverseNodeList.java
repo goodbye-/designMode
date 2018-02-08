f12a7a57829b165a219902830573d438a75fea3f
shui_st <767407960@qq.com>
2018-02-09 00:56:31 +0800

    优化1

--- src/com/sst/sington/ReverseNodeList.java
+++ src/com/sst/sington/ReverseNodeList.java
@@ -11,36 +11,52 @@ public class ReverseNodeList {
 		head.next.next.next.next.next.next = new Node(7);
 		System.out.println(head);
 		System.out.println(head);
-		Node result = reverse(head);
+		Node result = reverse1(head);
 		System.out.println(result);
 	}
-	
-	public static Node reverse(Node node){
-		
-		if(node == null || node.next == null){
-			return node; //如果node为空或者只有node一个节点，则返回node本身
+
+	public static Node reverse(Node node) {
+
+		if (node == null || node.next == null) {
+			return node; // 如果node为空或者只有node一个节点，则返回node本身
 		}
-		Node pp,pc,pn; //pp前一个节点，pc当前节点，pn下一个节点
+		Node pp, pc, pn; // pp前一个节点，pc当前节点，pn下一个节点
 		pp = node;
 		pc = node.next;
 		pn = pc.next;
 		pp.next = null;
-		while(pn != null){
+		while (pn != null) {
 			pc.next = pp;
 			pp = pc;
 			pc = pn;
 			pn = pn.next;
 		}
-		pc.next = pp; //如果最后pn为空，则循环里的第一句话，将当前节点指向前一个节点不能忘记。
+		pc.next = pp; // 如果最后pn为空，则循环里的第一句话，将当前节点指向前一个节点不能忘记。
 		return pc;
 	}
 
-	
-	
+	/**
+	 * @param node
+	 * 	之前方法太蠢，可以假设一个null节点指向node节点，就不用分情况了
+	 * @return
+	 */
+	public static Node reverse1(Node node) {
+
+		Node pp = null,pn,pc;
+		pc = node;
+		while(pc != null){
+			pn = pc.next; //记录下一个节点
+			pc.next = pp;//当前节点只想前一个节点
+			pp = pc;//当前节点变成前一个节点
+			pc = pn;//下一个节点变成当前节点
+		}
+		//等到下一个当前节点为空时，将前一个节点也就是正序的最后一个节点返回，妈的，变得这么简洁？？
+		return pp;
+	}
+
 }
 
-
-class Node implements Cloneable{
+class Node implements Cloneable {
 
 	public int index;
 	public Node next;
@@ -50,9 +66,10 @@ class Node implements Cloneable{
 		this.index = index;
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 * 原先的头String直接用node会破坏结构，所以clone了一个
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString() 原先的头String直接用node会破坏结构，所以clone了一个
 	 */
 	@Override
 	public String toString() {
